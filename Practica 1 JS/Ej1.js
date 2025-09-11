e2f1c540178d2f0bb3092460570aac297d58f8fb
Aimar Medina <142GA03@ARRIAGAINFO.LOCAL>
2025-09-11 12:51:42 +0200

    primer commit y dos ejercicios hehchos

---
+++ Practica 1 JS/Ej1.js
@@ -0,0 +1,11 @@
+var n1 = Number(16);
+n1 = Number(16.678);
+var parteEntera = Math.floor(n1);
+var parteDecimal = n1 - parteEntera;
+var redondeoArriba = Math.ceil(n1);
+var redondeoAbajo = Math.floor(n1);
+var redondeo = Math.round(n1);
+var dosDecimales = n1.toFixed(2);
+dosDecimales = Number(dosDecimales);
+
+
---
+++ Practica 1 JS/Ej2.js
@@ -0,0 +1,12 @@
+var texto1 = "Hola"
+console.log(texto1.toUpperCase())
+console.log(texto1.charAt(1))
+console.log(texto1.charAt(3))
+console.log(texto1.includes("j"))
+console.log(texto1.indexOf("o"))
+console.log(texto1.length)
+
+var texto2 = texto1 + " buenos días";
+console.log(texto2)
+console.log(texto2.substring(3,6))
+
