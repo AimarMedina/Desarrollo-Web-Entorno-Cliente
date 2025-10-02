3ce42af7d890119ff9aa6508b4dc4375e8607066
Aimar Medina <aimar.medina@ikasle.egibide.org>
2025-10-02 23:18:12 +0200

    terminado ejercicio 1 de la practica 2 de HTML+CSS

--- Tema2/Practica 2 HTML+JS/script.js
+++ Tema2/Practica 2 HTML+JS/script.js
@@ -1,19 +1,26 @@
 
 var añadirProductoButton = document.querySelector("#añadirProducto")
 var añadirProductoInput = document.querySelector("#producto")
+var listaProductosDOM = document.querySelector("#listaProductos")
+var vaciarListaButton = document.querySelector("#resetLista")
+var elementoLista = document.getElementsByTagName("li")
+
+var listaProductos = actualizarLista()
+
+crearLista()
 
 function actualizarLista(){
     if(typeof localStorage !== "undefined"){
-        return lista = JSON.parse(localStorage.getItem("productos"));
+        var lista = JSON.parse(localStorage.getItem("productos"));
+        return lista ? lista : [];
     }
+    return []
 }
 
 function añadirProducto(event){
     event.preventDefault()
 
-
-
-    let producto = document.querySelector("#producto").value
+    var producto = document.querySelector("#producto").value
     if(producto == ""){
         alert("Debes introducir un producto")
     }
@@ -23,20 +30,70 @@ function añadirProducto(event){
         localStorage.setItem("productos", JSON.stringify(productos))
         añadirProductoInput.value = ""
         actualizarLista()
+        crearLista()
     }
 
 }
 
 function crearLista(){
     var lista = actualizarLista()
+        reinicarLista()
+    if (lista){
+        lista.forEach(producto => {
+            var productoLista = document.createElement("li")
+            productoLista.textContent = producto
+            productoLista.addEventListener("dblclick",eliminarUnProducto)
 
-    lista.forEach(producto => {
-        var productoLista = document.createElement("li")
-        productoLista.textContent = producto
-
-        document.querySelector("#lista").appendChild(productoLista)
-    });
+            listaProductosDOM.appendChild(productoLista)
+        });
+    }
 }
 
+function reinicarLista(){
+    var productosLista = listaProductosDOM.children
+
+    Array.from(productosLista).forEach(producto =>{
+        producto.remove();
+        
+    })
+}
+
+function vaciarlista(){
+    if(localStorage.getItem("productos")){
+        localStorage.removeItem("productos")
+    }
+    actualizarLista()
+    reinicarLista()
+}
+
+function eliminarUnProducto(event){
+    var lista = JSON.parse(localStorage.getItem("productos"));
+
+
+    var listaArray = []
+
+    Array.from(lista).forEach(elemento=>{
+        listaArray.push(elemento)
+    })
+
+    indiceElemento = listaArray.indexOf(event.target.textContent)
+
+    if(indiceElemento !== 1){
+        listaArray.splice(indiceElemento,1)
+    }
+
+    localStorage.setItem("productos", JSON.stringify(listaArray))
+
+    
+    if(!listaArray){
+        vaciarlista()
+    }
+    event.target.remove()
+}
+
+
 añadirProductoButton.addEventListener("click", añadirProducto)
+vaciarListaButton.addEventListener("click",vaciarlista)
+
+
 
