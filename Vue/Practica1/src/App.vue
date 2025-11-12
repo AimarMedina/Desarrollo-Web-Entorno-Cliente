97818d58369589fd26d46079d0654cbf1c017843
Aimar Medina <aimar.medina@ikasle.egibide.org>
2025-11-12 15:49:01 +0100

    ej1 terminado

---
+++ Vue/Practica1/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import ej1 from './components/Ej1.vue'
+</script>
+
+<template>
+  <ej1></ej1>
+</template>
+
+<style scoped></style>
---
+++ Vue/Practica1/src/components/Ej1.vue
@@ -0,0 +1,11 @@
+<script setup>
+    import {ref} from 'vue'
+    let contador = ref(0)
+</script>
+
+<template>
+    <h1>Contador: {{ contador }}</h1>
+    <button @click="contador++">Incrementar</button>
+    <button @click="contador--">Dencrementar</button>
+    <button @click="contador=0">Resetear</button>
+</template>
